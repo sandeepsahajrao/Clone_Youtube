53eb3731942ffab260d3bfee9339a5cc905e73bc
Sandeep Sahajrao <107790439+sandeepsahajrao@users.noreply.github.com>
2022-11-11 11:27:33 +0000

    fetch problem with origin access

--- src/component/FetchApi.js
+++ src/component/FetchApi.js
@@ -1,24 +1,19 @@
-import React from "react";
+
 import axios from "axios";
-
-
-const Main_url='https://youtube-v31.p.rapidapi.com';
-
+const Main_url = "https://youtube-v31.p.rapidapi.com";
 const options = {
-
-  url:Main_url,
+  url: Main_url,
   params: {
-    maxResults: '5'
+    maxResults: "5",
   },
   headers: {
-    'X-RapidAPI-Key': process.env.api_call,
-    'X-RapidAPI-Host': 'youtube-v31.p.rapidapi.com'
-  }
+    "X-RapidAPI-Key": '429cb24795mshd2a34df539d18bap1bdffdjsnf6e3f1c3ad4f',
+    "X-RapidAPI-Host": "youtube-v31.p.rapidapi.com",
+  },
 };
 
-export const FetchApi=async (url)=>{    
-    const {data}=await axios.get(`{$Main_url}/{$url}`,options)
-    console.log(data)
-    return data;
-}
+export const FetchApi = async (url) => {
+  const { data } = await axios.get(`${Main_url}/${url}`, options);
 
+  return data;
+};
--- src/component/Videos.jsx
+++ src/component/Videos.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
-
+import { FetchApi } from './FetchApi'
+import { Box } from '@mui/system'
+import { Stack } from '@mui/material'
+import {VideoCard,ChannelCard} from './index'
 const Videos = ({videos}) => {
-  console.log(videos)
-  return (
-    <div>Videos444</div>
+  return(
+    <Stack direction='row' flexWrap='warp' justifyContent='start' gap='2'>
+      {videos.map((items,index)=>{
+        <Box key={index}>
+          {items.id.videoId && <VideoCard video={items}/>}
+          {items.id.ChannelId && <ChannelCard channeldetails={items}/>}
+        </Box>
+      })}
+    </Stack>
   )
 }
 
