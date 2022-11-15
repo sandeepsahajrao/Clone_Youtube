a065d127e2fcaf9d308fdbcaf578e170ef265824
Sandeep Sahajrao <sandeepsahajrao98@gmail.com>
2022-11-15 14:22:23 +0000

    commited videoDetails pages

--- src/component/VideoDtails.jsx
+++ src/component/VideoDtails.jsx
@@ -1,8 +1,37 @@
-import React from 'react'
+import React from 'react';
+import { useState,useEffect } from 'react';
+import { Box,Typography,Stack } from '@mui/material';
+import { useParams,Link} from 'react-router-dom';
+import {Videos} from './index'
+import { FetchApi } from './FetchApi';
+import ReactPlayer from 'react-player'
+
 
 const VideoDtails = () => {
+  const [videoDetails, setvideoDetails] = useState(null)
+  const {id}=useParams();
+  useEffect(()=>{
+    FetchApi(`videos?part=snippet,statistics&id=${id}`)
+    .then((data)=>setvideoDetails(data.items[1]))
+  },[id]);
+  
+  // const {snippet}=videoDetails
+    // console.log(videoDetails.snippet)
   return (
-    <div>VideoDtails</div>
+    <Box minHeight={'95vh'}>
+      <Stack sx={{xs:'column',md:'row'}}>
+        <Box flex={1}>
+
+          <Box sx={{width:'100%',position:'sticky',top:'86px'}}>
+            <ReactPlayer url={`https://www.youtube.com/watch?v=${id}`} className='react-player'
+            controls />
+            <Typography variant='h5' >
+              {/* {snippet.title} */}
+            </Typography>
+          </Box>
+        </Box>
+      </Stack>
+    </Box>
   )
 }
 
