e1f79418411e40150ce10e6b1b8aee3557a60e6c
Sandeep Sahajrao <sandeepsahajrao98@gmail.com>
2022-11-20 07:40:23 +0000

    projet ok for lounch

--- src/component/Videos.jsx
+++ src/component/Videos.jsx
@@ -1,12 +1,14 @@
-import React from 'react'
+import React from 'react';
+import Loader from './Loader';
 // import { FetchApi } from './FetchApi'
 
 import { Stack ,Box} from '@mui/material'
 import {VideoCard,ChannelCard} from './index'
 const Videos = ({videos}) => {
   // console.log(videos)
+  if(!videos?.length) return <Loader />;
   return(
-    <Stack direction='row' flexWrap='wrap' justifyContent='start' gap='20px' borderRadius={'20px'} >
+    <Stack direction='row' flexWrap='wrap' justifyContent='start' gap='20px' borderRadius={'20px'} sx={{justifyContent:"center"}} >
       {videos.map((items,index)=>(
         <Box key={index}>
           {items.id.videoId && <VideoCard video={items}/>}
